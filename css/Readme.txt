41a945d34a73d299ffacb1ed38685e77e206cc2a
Gull Sher <mobilegull73@gmail.com>
2023-09-26 06:01:44 +0500

    aa

--- css/Readme.txt
+++ css/Readme.txt
@@ -1,4 +1,22 @@
 . ResponsiveImageSlider.html created by following youtube "https://www.youtube.com/watch?v=3ETHQcHzDlY " of 23:16min
 . For js following "https://swiperjs.com/get-started#add-swiper-html-layout"
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
+.
 
 
---
+++ js/ResponsiveImgSliderjs.js
@@ -0,0 +1,19 @@
+//   Initialize Swiper 
+
+ 
+ var swiper = new Swiper(".mySwiper", { 
+      slidesPerView: 1,
+      spaceBetween: 4,
+      slidesPerGroup: 1,
+      loop: true,
+      loopFillGroupWithBlank: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
